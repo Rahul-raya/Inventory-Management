2d63235b1f002b46fa02e8822ac329ee95abbefc
Rahul-raya <rahul.22bce8855@vitapstudent.ac.in>
2025-09-01 16:46:17 +0530

    exceptions handled

--- src/main/java/com/example/Inventory/Management/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/Inventory/Management/Exception/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.example.Inventory.Management.Exception;
 
-import com.example.Inventory.Management.Exception.ProductNotFoundException;
-import com.example.Inventory.Management.Exception.InsufficientStockException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
